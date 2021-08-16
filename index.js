a3ecab904496e8cab0ec57a4f12f0fa5d03d62fe
Yaneizy Castillo <yccastillo18@gmail.com>
2021-08-16 11:23:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// function totalBatteries(batteryBatches) {
+//     let totalSum = 0
+//     for (const batteryBatch of batteryBatches) {
+//         totalSum += batteryBatch.batteryBatches;
+//     }
+//     return totalSum;
+// }
+
+// console.log(totalBatteries.reduce(batteryBatches, totalSum));
+
+let totalBatteries = batteryBatches.reduce(function(total, batches){
+    return batches + total
+    }
+  )
+
+  ///////// ^^ anonymous function ^^ ////// 
